f366a53327d485efb0020c0ce21a052f8875ce70
Kelsey Jordahl <kjordahl@alum.mit.edu>
2025-07-15 17:16:24 -0700

    Use uv dynamic versioning

--- steganan/__init__.py
+++ steganan/__init__.py
@@ -4,8 +4,14 @@ Steganan
 Steganography in floating point data with NaN payloads
 """
 
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"  # Fallback for development mode
+
 __author__ = "Kelsey Jordahl"
-__version__ = "0.2.0"
 __all__ = [
     "decode_array",
     "encode_array",
